943072101925ffefc0c47b944c08b6918c4ba00b
Ethylene <61812743+Neutroooon@users.noreply.github.com>
2020-03-23 14:47:51 +0800

    更新了排列数功能

--- AICompute.java
+++ AICompute.java
@@ -17,4 +17,48 @@ public class AICompute{
         }
         System.out.println(sum);
     }
+    public static void Permutation(){
+        System.out.println("       _   n  \n" +
+                           "      / |     \n" +
+                           "     /--| m    ");
+
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Please write n:");
+        if(scan1.hasNextInt()){
+            int x1 = scan1.nextInt();
+            Scanner scan2 = new Scanner(System.in);
+            System.out.println("Please write m:");
+            if(scan2.hasNextInt()){
+                int x2 = scan2.nextInt();
+                int result = 1;
+                int result2 = 1;
+                if(x1==x2){
+                    for(int a=1;a<=x1;a++){
+                        result2 = result2*a;
+                    }
+                    System.out.println(result2);
+                }
+                else{
+                    if(x1<x2){
+                        for(int b=(x2-x1+1);b<=x2;b++){
+                            result = result*b;
+                        }
+                        System.out.println(result);
+                    }
+                    else{
+                        System.out.println("Error!");
+                        Permutation();
+                    }
+                }
+            }
+            else{
+                System.out.println("Please give a integer!");
+                Permutation();
+            }
+        }
+        else{
+            System.out.println("Please give a integer!");
+            Permutation();
+        }
+    }
 }
